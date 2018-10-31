cb7bb6ebdda44aa08f9d25a8d2dda5db0840f684
Jonathan Duffin <jonathan_duffin@hotmail.com>
2018-10-31 13:38:41 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ function happyHolidays() {
   return "Happy holidays!"
 };
 function happyHolidaysTo(name) {
-  return `Happy holidays, ${name}!`
+  return "Happy holidays, `name`!"
 };
 function happyHolidayTo(holiday, name) {
-  return `Happy £{holiday}, ${name}!`
+  return "Happy £{holiday}, `name`!"
 };
 function holidayCountdown(holiday, countdown) {
-  return `It's ${countdown} days until ${holiday}!`
+  return "It's ${countdown} days until `holiday`!"
 };
